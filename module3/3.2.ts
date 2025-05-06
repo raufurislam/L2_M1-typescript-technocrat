9c4f2b1ae10501617b43cf59540b4f06054e05ce
raufurislam <raufurislam@gmail.com>
2025-05-06 15:07:08 +0600

    3.2 oop inheritance

---
+++ module3/3.2.ts
@@ -0,0 +1,51 @@
+{
+  // oop inheritance
+  class Parent {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+
+    getSleep(numOfHours: number) {
+      console.log(`${this.name} will sleep for ${numOfHours}`);
+    }
+  }
+
+  class Student extends Parent {
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+  }
+
+  const student1 = new Student("Raufur", 23, "jp");
+
+  student1.getSleep(8);
+
+  class Teacher extends Parent {
+    designation: string;
+
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+      super(name, age, address);
+      this.designation = designation;
+    }
+
+    takeClass(numOfClass: number) {
+      console.log(`${this.name} will take class ${numOfClass}`);
+    }
+  }
+
+  const teacher = new Teacher("Mr. T", 51, "uganda", "Professor");
+  console.log({ student1 });
+  console.log({ teacher });
+  //
+}
