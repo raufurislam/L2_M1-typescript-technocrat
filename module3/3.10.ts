34154a6aa925fbda65a8bd75310adff1642a17b3
raufurislam <raufurislam@gmail.com>
2025-05-07 15:22:15 +0600

    3.10 Encapsulation in oop

--- module3/3.10.ts
+++ module3/3.10.ts
@@ -1,4 +1,33 @@
 {
-  //
+  // Encapsulation in oop
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    protected _balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    public addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    private getBalance() {
+      return this._balance;
+    }
+    getHiddenMthod() {
+      return this.getBalance();
+    }
+  }
+
+  const goribManusherAccount = new BankAccount(111, "Raufur", 50);
+  goribManusherAccount.addDeposit(60);
+  const myBalance = goribManusherAccount.getHiddenMthod();
+
+  console.log(goribManusherAccount);
+  console.log({ myBalance });
   //
 }
